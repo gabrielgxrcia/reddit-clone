d9ec0b6799f00531853226970d91e3d48c95e20f
Gabriel <gabrielgrazeffi123@gmail.com>
2023-07-14 16:57:13 -0300

    voting for posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,24 +1,44 @@
+"use client"
+
 import { formatTimeToNow } from "@/lib/utils"
 import { Post, User, Vote } from "@prisma/client"
 import { MessageSquare } from "lucide-react"
+import Link from "next/link"
 import { FC, useRef } from "react"
 import EditorOutput from "./EditorOutput"
+import PostVoteClient from "./post-vote/PostVoteClient"
+
+type PartialVote = Pick<Vote, "type">
 
 interface PostProps {
-  subredditName: string
   post: Post & {
     author: User
     votes: Vote[]
   }
+  votesAmt: number
+  subredditName: string
+  currentVote?: PartialVote
   commentAmt: number
 }
 
-const Post: FC<PostProps> = ({ subredditName, post, commentAmt }) => {
-  const pRef = useRef<HTMLDivElement>(null)
+const Post: FC<PostProps> = ({
+  post,
+  votesAmt: _votesAmt,
+  currentVote: _currentVote,
+  subredditName,
+  commentAmt,
+}) => {
+  const pRef = useRef<HTMLParagraphElement>(null)
 
   return (
     <div className="rounded-md bg-white shadow">
       <div className="px-6 py-4 flex justify-between">
+        <PostVoteClient
+          postId={post.id}
+          initialVotesAmt={_votesAmt}
+          initialVote={_currentVote?.type}
+        />
+
         <div className="w-0 flex-1">
           <div className="max-h-40 mt-1 text-xs text-gray-500">
             {subredditName ? (
@@ -32,10 +52,9 @@ const Post: FC<PostProps> = ({ subredditName, post, commentAmt }) => {
                 <span className="px-1">•</span>
               </>
             ) : null}
-            <span>Posted by u/{post.author.name}</span>{" "}
+            <span>Posted by u/{post.author.username}</span>{" "}
             {formatTimeToNow(new Date(post.createdAt))}
           </div>
-
           <a href={`/r/${subredditName}/post/${post.id}`}>
             <h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
               {post.title}
@@ -47,24 +66,23 @@ const Post: FC<PostProps> = ({ subredditName, post, commentAmt }) => {
             ref={pRef}
           >
             <EditorOutput content={post.content} />
-
             {pRef.current?.clientHeight === 160 ? (
-              <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent" />
+              // blur bottom if content is too long
+              <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent"></div>
             ) : null}
           </div>
         </div>
       </div>
 
-      <div className="bg-gray-50 z-20 text-sm p-4 sm:px-6">
-        <a
-          className="w-fit flex items-center gap-2"
+      <div className="bg-gray-50 z-20 text-sm px-4 py-4 sm:px-6">
+        <Link
           href={`/r/${subredditName}/post/${post.id}`}
+          className="w-fit flex items-center gap-2"
         >
           <MessageSquare className="h-4 w-4" /> {commentAmt} comments
-        </a>
+        </Link>
       </div>
     </div>
   )
 }
-
 export default Post
